c2741f047b0b7de91abed2cb6c68a9e33f723ebc
SerlioGiron <serlio.giron@unitec.edu>
2024-03-15 16:24:52 -0600

    first commit

---
+++ home.py
@@ -0,0 +1,20 @@
+from tkinter import *
+import customtkinter
+
+root = customtkinter.CTk()
+
+
+button = customtkinter.CTkButton( master=root, text="1")
+button2 = customtkinter.CTkButton( master=root, text="2")
+button3 = customtkinter.CTkButton( master=root, text="3")
+button4 = customtkinter.CTkButton( master=root, text="4")
+button5 = customtkinter.CTkButton( master=root, text="5")
+
+
+button.place(relx=0.5, rely=0.1, anchor=CENTER)
+button2.place(relx=0.5, rely=0.25, anchor=CENTER)
+button3.place(relx=0.5, rely=0.5, anchor=CENTER)
+button4.place(relx=0.5, rely=0.75, anchor=CENTER)
+button5.place(relx=0.5, rely=0.9, anchor=CENTER)
+
+root.mainloop()
