8aa07d810c12877ef1bdf1c0feb2c0b1a0929132
SerlioGiron <serlio.giron@unitec.edu>
2024-03-16 20:39:16 -0600

    algunos cambios

--- raspaygana.py
+++ raspaygana.py
@@ -25,6 +25,17 @@ def on_button_click(button):
     button.configure(fg_color="red", text = random_number)
     print("Button clicked")
     print(cont)
+    
+    if cont == 2 and historial[0] != historial[1]:
+        reiniciar()
+        historial.clear()
+        print("Perdiste")
+        WindowLose = customtkinter.CTk()
+        WindowLose.anchor = CENTER
+        pantWin = customtkinter.CTkLabel(master=WindowLose, text="Perdiste", fg_color="red")
+        pantWin.place(relx=0.5, rely=0.5, anchor=CENTER)
+        WindowLose.mainloop()
+    
     if cont >= 3:
         for numero in historial:
             print( "numero -> ",numero)
@@ -66,23 +77,23 @@ def main():
     # ctk = customtkinter.CTk()
     global botones
 
-    button1 = customtkinter.CTkButton( master=root, text="", command=lambda: on_button_click(button1))
+    button1 = customtkinter.CTkButton( master=root, text="", command=lambda: on_button_click(button1), fg_color="lightblue")
     botones.append(button1)
-    button2 = customtkinter.CTkButton( master=root, text="", command=lambda: on_button_click(button2))
+    button2 = customtkinter.CTkButton( master=root, text="", command=lambda: on_button_click(button2), fg_color="lightblue")
     botones.append(button2)
-    button3 = customtkinter.CTkButton( master=root, text="", command=lambda: on_button_click(button3))
+    button3 = customtkinter.CTkButton( master=root, text="", command=lambda: on_button_click(button3), fg_color="lightblue")
     botones.append(button3)
-    button4 = customtkinter.CTkButton( master=root, text="", command=lambda: on_button_click(button4))
+    button4 = customtkinter.CTkButton( master=root, text="", command=lambda: on_button_click(button4), fg_color="lightblue")
     botones.append(button4)
-    button5 = customtkinter.CTkButton( master=root, text="", command=lambda: on_button_click(button5))
+    button5 = customtkinter.CTkButton( master=root, text="", command=lambda: on_button_click(button5), fg_color="lightblue")
     botones.append(button5)
-    button6 = customtkinter.CTkButton( master=root, text="", command=lambda: on_button_click(button6))
+    button6 = customtkinter.CTkButton( master=root, text="", command=lambda: on_button_click(button6), fg_color="lightblue")
     botones.append(button6)
-    button7 = customtkinter.CTkButton( master=root, text="", command=lambda: on_button_click(button7))
+    button7 = customtkinter.CTkButton( master=root, text="", command=lambda: on_button_click(button7), fg_color="lightblue")
     botones.append(button7)
-    button8 = customtkinter.CTkButton( master=root, text="", command=lambda: on_button_click(button8))
+    button8 = customtkinter.CTkButton( master=root, text="", command=lambda: on_button_click(button8), fg_color="lightblue")
     botones.append(button8)
-    button9 = customtkinter.CTkButton( master=root, text="", command=lambda: on_button_click(button9))
+    button9 = customtkinter.CTkButton( master=root, text="", command=lambda: on_button_click(button9), fg_color="lightblue")
     botones.append(button9)
     
     button1.place(relx=0.3, rely=0.25, anchor=CENTER)
