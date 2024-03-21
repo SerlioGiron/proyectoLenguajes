cff6c6a9551b8d7903652af4c558e6fb010203d6
DiegoMolinaaa <diego.molina0801@gmail.com>
2024-03-21 01:21:35 -0600

    Cambios en el hangman

--- home.py
+++ home.py
@@ -3,21 +3,21 @@ import customtkinter
 import raspaygana
 import sudoku
 import CarreraTortugas
+import hangman
 import wordle
 import Buscaminas
 
+
 def main(): 
     root = customtkinter.CTk()
-    root.geometry("800x480")
+    root.geometry("800x400")
     root.title("Home")
     root.anchor = CENTER
-
     button = customtkinter.CTkButton( master=root, text="Raspa y Gana", command=raspaygana.main)
     button2 = customtkinter.CTkButton( master=root, text="Sudoku", command=sudoku.main)
     button3 = customtkinter.CTkButton( master=root, text="Carrera de Tortugas", command=CarreraTortugas.main)
     button4 = customtkinter.CTkButton( master=root, text="Buscaminas", command=Buscaminas.main)
-    button5 = customtkinter.CTkButton( master=root, text="5")
-
+    button5 = customtkinter.CTkButton(master=root, text="Hangman", command=hangman.main)
     button.place(relx=0.5, rely=0.1, anchor=CENTER)
     button2.place(relx=0.5, rely=0.25, anchor=CENTER)
     button3.place(relx=0.5, rely=0.5, anchor=CENTER)
